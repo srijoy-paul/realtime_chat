33a2b84d55f1af6c6d3b20aa234e792605e7bc81
Srijoy Paul <srijoykumar1507@gmail.com>
2024-01-06 23:20:40 +0530

    bulk_commit_1

---
+++ chat_web/chat_web_frontend/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
+# Create your views here.
+def frontpage(request):
+    return render(request, 'chat_web_frontend/frontpage.html')
+
+def homepage(request):
+    return render(request,'chat_web_frontend/home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'chat_web_frontend/signup.html', {'form': form})
